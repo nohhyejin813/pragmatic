69002369c0d447ffcf422195c6f0b78fec9c8376
GCHEALTHCARE\hjnoh <gchjnoh@gccorp.com>
2023-06-23 11:43:54 +0900

    django course 40 commit

--- static/js/magicgrid.js
+++ static/js/magicgrid.js
@@ -288,12 +288,13 @@ MagicGrid.prototype.listen = function listen () {
 let magicGrid = new MagicGrid({
   container: '.container',
   animate: true,
-  gutter: 30,
+  gutter: 10,
   static: true,
   useMin: true
 });
 
 var masonrys = document.getElementsByTagName("img");
+
 for (let i=0; i< masonrys.length; i++){
   masonrys[i].addEventListener('load', function () {
     magicGrid.positionItems();
